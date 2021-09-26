29e0aa515a899632be74681f361bc8e19bc5bf8c
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2021-09-26 17:43:47 +0800

    Remove variable pitch in org mode and use Iosevka 15 for font

--- personal/org-mode-config.el
+++ personal/org-mode-config.el
@@ -2,24 +2,24 @@
 ;;; Code:
 (prelude-require-packages '(org-bullets org-roam))
 
-(dolist (face '((org-level-1 . 1.2)
-                (org-level-2 . 1.1)
-                (org-level-3 . 1.05)
-                (org-level-4 . 1.0)
-                (org-level-5 . 1.1)
-                (org-level-6 . 1.1)
-                (org-level-7 . 1.1)
-                (org-level-8 . 1.1)))
-  (set-face-attribute (car face) nil :font "Times New Roman" :weight 'regular :height (cdr face)))
+;; (dolist (face '((org-level-1 . 1.2)
+;;                 (org-level-2 . 1.1)
+;;                 (org-level-3 . 1.05)
+;;                 (org-level-4 . 1.0)
+;;                 (org-level-5 . 1.1)
+;;                 (org-level-6 . 1.1)
+;;                 (org-level-7 . 1.1)
+;;                 (org-level-8 . 1.1)))
+;;   (set-face-attribute (car face) nil :font "Times New Roman" :weight 'regular :height (cdr face)))
 
-  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
-(set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
-(set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-(set-face-attribute 'org-checkbox nil :inherit 'fixed-)
+;;   ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+;; (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+;; (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+;; (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+;; (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+;; (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+;; (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+;; (set-face-attribute 'org-checkbox nil :inherit 'fixed-)
 
 (setq org-ellipsis " ▾"
       org-agenda-start-with-log-mode t
@@ -38,5 +38,5 @@
 
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)
                            (org-indent-mode 1)
-                           (variable-pitch-mode 1)
+                           ;; (variable-pitch-mode 1)
                            (visual-line-mode 1)))
--- personal/preload/ui-config.el
+++ personal/preload/ui-config.el
@@ -4,6 +4,8 @@
 (defvar cubeg/default-variable-font-size 150)
 (defvar cubeg/default-modeline-font-size 130)
 
-(set-face-attribute 'default nil :font "Menlo" :height cubeg/default-font-size)
-(set-face-attribute 'fixed-pitch nil :font "Menlo" :height cubeg/default-font-size)
+(set-face-attribute 'default nil :font "Iosevka" :height cubeg/default-font-size)
+(set-face-attribute 'fixed-pitch nil :font "Iosevka" :height cubeg/default-font-size)
 (set-face-attribute 'variable-pitch nil :font "Times New Roman" :height cubeg/default-variable-font-size :weight 'regular)
+
+(setq prelude-theme 'tsdh-light)
