9ac3680225ab158e342fddb99f0b60de4aa524fc
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2021-10-07 09:18:46 +0800

    use Monaco font

--- personal/preload/ui-config.el
+++ personal/preload/ui-config.el
@@ -1,11 +1,11 @@
 ;;; Commentary:
 
-(defvar cubeg/default-font-size 150)
-(defvar cubeg/default-variable-font-size 150)
-(defvar cubeg/default-modeline-font-size 130)
+(defvar cubeg/default-font-size 140)
+(defvar cubeg/default-variable-font-size 140)
+(defvar cubeg/default-modeline-font-size 120)
 
-(set-face-attribute 'default nil :font "Iosevka" :height cubeg/default-font-size)
-(set-face-attribute 'fixed-pitch nil :font "Iosevka" :height cubeg/default-font-size)
+(set-face-attribute 'default nil :font "Menlo" :height cubeg/default-font-size)
+(set-face-attribute 'fixed-pitch nil :font "Menlo" :height cubeg/default-font-size)
 (set-face-attribute 'variable-pitch nil :font "Times New Roman" :height cubeg/default-variable-font-size :weight 'regular)
 
 (setq prelude-theme 'tsdh-light)
