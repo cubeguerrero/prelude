7916d93d3f766475efce3b9e666fb40f48eb428b
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2021-10-08 18:18:49 +0800

    Use JetBrains and turn on mac-auto-operator-composition-mode

--- personal/preload/ui-config.el
+++ personal/preload/ui-config.el
@@ -4,8 +4,8 @@
 (defvar cubeg/default-variable-font-size 140)
 (defvar cubeg/default-modeline-font-size 120)
 
-(set-face-attribute 'default nil :font "Menlo" :height cubeg/default-font-size)
-(set-face-attribute 'fixed-pitch nil :font "Menlo" :height cubeg/default-font-size)
+(set-face-attribute 'default nil :font "JetBrains Mono" :height cubeg/default-font-size)
+(set-face-attribute 'fixed-pitch nil :font "JetBrains Mono" :height cubeg/default-font-size)
 (set-face-attribute 'variable-pitch nil :font "Times New Roman" :height cubeg/default-variable-font-size :weight 'regular)
 
-(setq prelude-theme 'tsdh-light)
+(mac-auto-operator-composition-mode t)
