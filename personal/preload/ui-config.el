915559a291db21787756cf583521fc417b34eaa0
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2021-10-10 19:26:25 +0800

    Iosevka and nil theme

--- personal/preload/ui-config.el
+++ personal/preload/ui-config.el
@@ -4,9 +4,10 @@
 (defvar cubeg/default-variable-font-size 140)
 (defvar cubeg/default-modeline-font-size 120)
 
-(set-face-attribute 'default nil :font "JetBrains Mono" :height cubeg/default-font-size)
-(set-face-attribute 'fixed-pitch nil :font "JetBrains Mono" :height cubeg/default-font-size)
+(set-face-attribute 'default nil :font "Iosevka" :height cubeg/default-font-size)
+(set-face-attribute 'fixed-pitch nil :font "Iosevka" :height cubeg/default-font-size)
 (set-face-attribute 'variable-pitch nil :font "Times New Roman" :height cubeg/default-variable-font-size :weight 'regular)
 
 (mac-auto-operator-composition-mode t)
 (scroll-bar-mode -1)
+(setq prelude-theme nil)
