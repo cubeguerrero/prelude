4086df05f1679de980f8ff83ff3e67d7083b9267
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2021-11-28 22:06:09 +0800

    Use JetBrains Mono

--- personal/preload/ui-config.el
+++ personal/preload/ui-config.el
@@ -1,11 +1,11 @@
 ;;; Commentary:
 
-(defvar cubeg/default-font-size 150)
-(defvar cubeg/default-variable-font-size 150)
-(defvar cubeg/default-modeline-font-size 130)
+(defvar cubeg/default-font-size 140)
+(defvar cubeg/default-variable-font-size 140)
+(defvar cubeg/default-modeline-font-size 120)
 
-(set-face-attribute 'default nil :font "Inconsolata LGC" :height cubeg/default-font-size)
-(set-face-attribute 'fixed-pitch nil :font "Inconsolata LGC" :height cubeg/default-font-size)
+(set-face-attribute 'default nil :font "JetBrains Mono" :height cubeg/default-font-size)
+(set-face-attribute 'fixed-pitch nil :font "JetBrains Mono" :height cubeg/default-font-size)
 (set-face-attribute 'variable-pitch nil :font "Times New Roman" :height cubeg/default-variable-font-size :weight 'regular)
 
 (mac-auto-operator-composition-mode t)
